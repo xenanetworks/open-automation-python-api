65e082ae55d6126c43ce5d1491afffbd03049d91
Leonard Yu <hyu@xenanetworks.com>
2022-03-26 22:24:05 +0100

    fixed doc version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ author = u'Artem Constantinov, Ron Ding, Leonard Yu'
 
 
 # The full version, including alpha/beta/rc tags.
-release = '1.0'
+release = '1.0b3'
 # The short X.Y version.
-version = '1.0b3'
+version = '1.0'
 
 
 # -- General configuration -----------------------------------------------------
