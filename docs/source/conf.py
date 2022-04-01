7099cc2bc37d686f926c39a82a1ed68220292807
Leonard Yu <hyu@xenanetworks.com>
2022-04-01 22:20:42 +0200

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,7 +125,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [(master_doc, 'xoadriverdoc.tex', title + ' ' + release, author, 'manual'),
+latex_documents = [(master_doc, 'xoadriverdoc.tex', title, author, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
