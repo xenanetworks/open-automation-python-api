8a6cad5c90ee485ea007c1e28c6517b8ea4eab85
Leonard Yu <hyu@xenanetworks.com>
2022-04-29 23:26:51 +0200

    Fixed typos in exceptions

--- xoa_driver/internals/exceptions/modules.py
+++ xoa_driver/internals/exceptions/modules.py
@@ -5,7 +5,7 @@ class WrongModuleError(Exception):
         self.module_revision = module_revision
         self.allowed_revisions = allowed_revisions
         self.msg = (
-            f"Module of revision <{self.module_revision}> can not be asign to the current tester,\n"
-            f"Permited only modules of next revisions {allowed_revisions}"
+            f"Module of revision <{self.module_revision}> can not be assign to the current tester,\n"
+            f"Permitted only modules of next revisions {allowed_revisions}"
         )
         super().__init__(self.msg)
--- xoa_driver/internals/exceptions/testers.py
+++ xoa_driver/internals/exceptions/testers.py
@@ -1,11 +1,11 @@
 class WrongTesterError(Exception):
     def __init__(self) -> None:
-        self.msg = "Tester you trying to connect is of different type"
+        self.msg = "Tester you are trying to connect is of a different type."
         super().__init__(self.msg)
 
 class WrongTesterPasswordError(Exception):
     def __init__(self, password: str) -> None:
         self.password = password
-        self.msg = f"Can't create session on the tester because password is incorrect."
+        self.msg = f"Can't create session on the tester because the password is incorrect."
         super().__init__(self.msg)
 
