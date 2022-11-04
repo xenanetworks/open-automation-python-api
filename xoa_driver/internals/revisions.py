62a9448f6cbca6b17f3d24722196301b98fff7d8
Leonard Yu <hyu@xenanetworks.com>
2022-11-04 23:51:10 +0100

    refactor: Sort revision alphabetically

--- xoa_driver/internals/revisions.py
+++ xoa_driver/internals/revisions.py
@@ -1,30 +1,47 @@
 from xoa_driver import modules
 
-# region Valkyrie Modules
+# region Valkyrie Modules, sorting order: alphabetical
 
 valkyrie_modules = {
     # family_d
-    "Odin-1G-3S-6P-E": modules.MOdin1G3S6PE,
+    "Odin-1G-3S-2P-T": modules.MOdin1G3S2PT,
     "Odin-1G-3S-6P": modules.MOdin1G3S6P,
     "Odin-1G-3S-6P[b]": modules.MOdin1G3S6P_b,
-    "Odin-1G-3S-2P-T": modules.MOdin1G3S2PT,
+    "Odin-1G-3S-6P-E": modules.MOdin1G3S6PE,
+
+    # family_m
+    "Odin-1G-3S-6P-T1-RJ45": modules.MOdin1G3S6PT1RJ45,
     
     # family_e
     "Odin-5G-4S-6P-CU": modules.MOdin5G4S6PCU,
-    "Odin-10G-5S-6P-CU": modules.MOdin10G5S6PCU,
-    "Odin-10G-5S-6P-CU[b]": modules.MOdin10G5S6PCU_b,
-    "Odin-10G-3S-6P-CU": modules.MOdin10G3S6PCU,
-    "Odin-10G-3S-2P-CU": modules.MOdin10G3S2PCU,
-    
+
     # family_f
     "Odin-10G-1S-2P": modules.MOdin10G1S2P,
     "Odin-10G-1S-2P[b]": modules.MOdin10G1S2P_b,
     "Odin-10G-1S-2P[c]": modules.MOdin10G1S2P_c,
+    "Odin-10G-1S-2P[d]": modules.MOdin10G1S2P_d,
+    "Odin-10G-1S-2P-T": modules.MOdin10G1S2PT,
     "Odin-10G-1S-6P": modules.MOdin10G1S6P,
     "Odin-10G-1S-6P[b]": modules.MOdin10G1S6P_b,
-    "Odin-10G-1S-2P-T": modules.MOdin10G1S2PT,
-    "Odin-10G-1S-2P[d]": modules.MOdin10G1S2P_d,
     "Odin-10G-1S-12P": modules.MOdin10G1S12P,
+
+    # family_e
+    "Odin-10G-3S-2P-CU": modules.MOdin10G3S2PCU,
+    "Odin-10G-3S-6P-CU": modules.MOdin10G3S6PCU,
+
+    "Odin-10G-5S-6P-CU": modules.MOdin10G5S6PCU,
+    "Odin-10G-5S-6P-CU[b]": modules.MOdin10G5S6PCU_b,
+
+    # family_combi
+    "Odin-10G-4S-2P-Combi": modules.MOdin10G4S2PCombi,
+    "Odin-10G-4S-2P-Combi[b]": modules.MOdin10G4S2PCombi_b,
+    
+    # family_n
+    "Odin-40G-2S-2P": modules.MOdin40G2S2P,
+    "Odin-40G-2S-2P-B": modules.MOdin40G2S2PB,
+    
+    # family_h
+    "Odin-100G-3S-1P": modules.MThor400G7S1P_d,
     
     # family_g
     "Loki-100G-3S-1P": modules.MLoki100G3S1P,
@@ -33,8 +50,6 @@ valkyrie_modules = {
     
     # family_h
     "Loki-100G-5S-1P": modules.MLoki100G5S1P,
-    "Odin-100G-3S-1P": modules.MThor400G7S1P_d,
-    
     
     # family_i
     "Loki-100G-5S-2P": modules.MLoki100G5S2P,
@@ -58,16 +73,6 @@ valkyrie_modules = {
     "Freya-800G-4S-1P[b]": modules.MFreya800G4S1P_b,
     "Freya-800G-4S-1P-OSFP[a]": modules.MFreya800G4S1POSFP_a,
     
-    # family_m
-    "Odin-1G-3S-6P-T1-RJ45": modules.MOdin1G3S6PT1RJ45,
-    
-    # family_n
-    "Odin-40G-2S-2P": modules.MOdin40G2S2P,
-    
-    # family_combi
-    "Odin-10G-4S-2P-Combi": modules.MOdin10G4S2PCombi,
-    "Odin-10G-4S-2P-Combi[b]": modules.MOdin10G4S2PCombi_b,
-    
 }
 
 # endregion
@@ -75,7 +80,9 @@ valkyrie_modules = {
 # region Chimera Modules
 
 chimera_modules = {
-    "Chimera-100G-5S-2P": modules.ModuleChimera,
+    "Chimera-100G-5S-2P": modules.MChi100G5S2P,
+    "Chimera-100G-5S-2P[b]": modules.MChi100G5S2P_b,
+    "Chimera-40G-5S-2P": modules.MChi40G5S2P,
 }
 
 # endregion
