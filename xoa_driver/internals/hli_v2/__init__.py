7d52fdc5129a387dd4f80b2950e40062a410032e
Leonard Yu <hyu@xenanetworks.com>
2023-03-23 11:41:00 +0100

    style: Update warning style

--- xoa_driver/internals/hli_v2/__init__.py
+++ xoa_driver/internals/hli_v2/__init__.py
@@ -1,4 +1,5 @@
 import warnings
 
-warnings.formatwarning = lambda message, category, filename, lineno, *_: f"\n\33[33m{category.__name__}\33[0m: {message}\n\n"
-warnings.warn("The HLIv2 is under havy development, and api can be changet dramaticly in the future.", ResourceWarning)
+warnings.simplefilter('always', ResourceWarning)
+warnings.formatwarning = lambda message, category, filename, lineno, *_: f"\n\33[103m{category.__name__}\33[0m: {message}\n\n"
+warnings.warn("xoa_driver.v2 is under development and it subject to changes without notice.", ResourceWarning)
