99c7a7e397bcb412528a5ab58038196f743668b8
Leonard Yu <hyu@xenanetworks.com>
2023-03-28 16:22:41 +0200

    fix: xoa_driver.v2 warning

--- xoa_driver/internals/hli_v2/__init__.py
+++ xoa_driver/internals/hli_v2/__init__.py
@@ -1,5 +1,3 @@
-import warnings
 
-warnings.simplefilter('always', ResourceWarning)
-warnings.formatwarning = lambda message, category, filename, lineno, *_: f"\n\33[103m{category.__name__}\33[0m: {message}\n\n"
-warnings.warn("xoa_driver.v2 is under development and it subject to changes without notice.", ResourceWarning)
+
+
