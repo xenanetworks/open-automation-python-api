4b7b43fb95fb07b2e4af24365d02e71cdb66a9ab
Leonard Yu <hyu@xenanetworks.com>
2023-05-24 19:56:19 +0200

    Fix C_TKSTATUSEXT docstring

--- xoa_driver/internals/commands/c_commands.py
+++ xoa_driver/internals/commands/c_commands.py
@@ -1911,21 +1911,25 @@ class C_TKSTATUSEXT:
     class GetDataAttr(ResponseBodyStruct):
         status_string: str = field(XmpStr())
         """string, extended status in JSON format. The string is formatted as shown in the example below.
-        {
-            "FormatVersion": 1,
-            "ApplicationVersion": 452.0,
-            "TimeKeeperStatus":
-            {
-                "systemtimingstatus": "Waiting for good time source",
-                "syncsource": "NTP",
-                "sourcestate": "NTP server 10.0.0.110",
-                "sourceaccuracy": " No updates yet",
-                "versioninfo": "8.0.3",
-                "timesincestart": "0 day(s) 0 hours 1 minutes",
-                "timesinceboot": "0 day(s) 0 hours 2 minutes",
-                "updatetime": 1637916837
-            }
-        }
+            
+            .. code-block::json
+            
+                {
+                    "FormatVersion": 1,
+                    "ApplicationVersion": 452.0,
+                    "TimeKeeperStatus":
+                    {
+                        "systemtimingstatus": "Waiting for good time source",
+                        "syncsource": "NTP",
+                        "sourcestate": "NTP server 10.0.0.110",
+                        "sourceaccuracy": " No updates yet",
+                        "versioninfo": "8.0.3",
+                        "timesincestart": "0 day(s) 0 hours 1 minutes",
+                        "timesinceboot": "0 day(s) 0 hours 2 minutes",
+                        "updatetime": 1637916837
+                    }
+                }
+
         """
 
     def get(self) -> Token[GetDataAttr]:
