5eaa1b1f528ec6ef1b513d8af79154bf7ab75d59
Leonard Yu <leonard.yu@teledyne.com>
2024-07-05 16:06:27 +0200

    Fix forward referene

--- xoa_driver/modules.py
+++ xoa_driver/modules.py
@@ -123,6 +123,116 @@ from .internals.hli_v1.modules.module_l47ve import ModuleL47VE
 import typing
 
 Z10OdinModule = typing.Union[
+    MOdin1G3S2PT,
+    MOdin1G3S6P,
+    MOdin1G3S6P_b,
+    MOdin1G3S6PE,
+    MOdin1G3S6PT1RJ45,
+    MOdin10G4S2PCombi,
+    MOdin10G4S2PCombi_b,
+    MOdin5G4S6PCU,
+    MOdin10G1S2P,
+    MOdin10G1S2P_b,
+    MOdin10G1S2P_c,
+    MOdin10G1S2P_d,
+    MOdin10G1S2PT,
+    MOdin10G1S6P,
+    MOdin10G1S6P_b,
+    MOdin10G1S12P,
+    MOdin10G3S6PCU,
+    MOdin10G3S2PCU,
+    MOdin10G5S6PCU,
+    MOdin10G5S6PCU_b,
+    MOdin10G6S6P_a,
+    MOdin40G2S2P,
+    MOdin40G2S2PB,
+    MOdin100G3S1P,
+]
+
+Z100LokiModule = typing.Union[
+    MLoki100G3S1P,
+    MLoki100G3S1P_b,
+    MLoki100G3S1PSE,
+    MLoki100G3S1PB,
+    MLoki100G3S1PB_b,
+    MLoki100G5S1P,
+    MLoki100G5S2P,
+]
+
+Z400ThorModule = typing.Union[
+    MThor100G5S4P,
+    MThor400G7S1P,
+    MThor400G7S1PLE,
+    MThor400G7S1P_b,
+    MThor400G7S1P_c,
+    MThor400G7S1P_d,
+]
+
+Z800FreyaModule = typing.Union[
+    MFreya800G1S1P_a,
+    MFreya800G1S1P_b,
+    MFreya800G1S1POSFP_a,
+    MFreya800G1S1POSFP_b,
+    MFreya800G4S1P_a,
+    MFreya800G4S1P_b,
+    MFreya800G4S1P_c,
+    MFreya800G4S1P_d,
+    MFreya800G4S1P_e,
+    MFreya800G4S1P_f,
+    MFreya800G4S1POSFP_a,
+    MFreya800G4S1POSFP_b,
+    MFreya800G4S1POSFP_c,
+    MFreya800G4S1POSFP_d,
+    MFreya800G4S1POSFP_e,
+    MFreya800G4S1POSFP_f,
+    MFreya800G1S1P_a_g1,
+    MFreya800G1S1P_b_g1,
+    MFreya800G1S1POSFP_a_g1,
+    MFreya800G1S1POSFP_b_g1,
+    MFreya800G4S1P_a_g1,
+    MFreya800G4S1P_b_g1,
+    MFreya800G4S1P_c_g1,
+    MFreya800G4S1P_d_g1,
+    MFreya800G4S1P_e_g1,
+    MFreya800G4S1P_f_g1,
+    MFreya800G4S1POSFP_a_g1,
+    MFreya800G4S1POSFP_b_g1,
+    MFreya800G4S1POSFP_c_g1,
+    MFreya800G4S1POSFP_d_g1,
+    MFreya800G4S1POSFP_e_g1,
+    MFreya800G4S1POSFP_f_g1,
+    MFreya800G1S1P_a_g2,
+    MFreya800G1S1P_b_g2,
+    MFreya800G1S1POSFP_a_g2,
+    MFreya800G1S1POSFP_b_g2,
+    MFreya800G4S1P_a_g2,
+    MFreya800G4S1P_b_g2,
+    MFreya800G4S1P_c_g2,
+    MFreya800G4S1P_d_g2,
+    MFreya800G4S1P_e_g2,
+    MFreya800G4S1P_f_g2,
+    MFreya800G4S1POSFP_a_g2,
+    MFreya800G4S1POSFP_b_g2,
+    MFreya800G4S1POSFP_c_g2,
+    MFreya800G4S1POSFP_d_g2,
+    MFreya800G4S1POSFP_e_g2,
+    MFreya800G4S1POSFP_f_g2,
+]
+
+E100ChimeraModule = typing.Union[
+    ModuleChimera,
+    MChi100G5S2P,
+    MChi100G5S2P_b,
+    MChi40G2S2P,
+]
+
+LegacyModule = typing.Union[
+    ModuleL23VE,
+    ModuleL47,
+    ModuleL47VE,
+]
+
+GenericL23Module = typing.Union[
     "MOdin1G3S2PT",
     "MOdin1G3S6P",
     "MOdin1G3S6P_b",
@@ -147,9 +257,7 @@ Z10OdinModule = typing.Union[
     "MOdin40G2S2P",
     "MOdin40G2S2PB",
     "MOdin100G3S1P",
-]
 
-Z100LokiModule = typing.Union[
     "MLoki100G3S1P",
     "MLoki100G3S1P_b",
     "MLoki100G3S1PSE",
@@ -157,18 +265,14 @@ Z100LokiModule = typing.Union[
     "MLoki100G3S1PB_b",
     "MLoki100G5S1P",
     "MLoki100G5S2P",
-]
 
-Z400ThorModule = typing.Union[
     "MThor100G5S4P",
     "MThor400G7S1P",
     "MThor400G7S1PLE",
     "MThor400G7S1P_b",
     "MThor400G7S1P_c",
     "MThor400G7S1P_d",
-]
 
-Z800FreyaModule = typing.Union[
     "MFreya800G1S1P_a",
     "MFreya800G1S1P_b",
     "MFreya800G1S1POSFP_a",
@@ -219,30 +323,17 @@ Z800FreyaModule = typing.Union[
     "MFreya800G4S1POSFP_f_g2",
 ]
 
-E100ChimeraModule = typing.Union[
+GenericAnyModule = typing.Union[
+    GenericL23Module,
+    
+    "ModuleL23VE",
+    "ModuleL47",
+    "ModuleL47VE",
+
     "ModuleChimera",
     "MChi100G5S2P",
     "MChi100G5S2P_b",
     "MChi40G2S2P",
-]
-
-GenericL23Module = typing.Union[
-    Z10OdinModule,
-    Z100LokiModule,
-    Z400ThorModule,
-    Z800FreyaModule,    
-]
-
-LegacyModule = typing.Union[
-    "ModuleL23VE",
-    "ModuleL47",
-    "ModuleL47VE",
-]
-
-GenericAnyModule = typing.Union[
-    GenericL23Module,
-    LegacyModule,
-    E100ChimeraModule,
 ]
 
 __all__ = (
